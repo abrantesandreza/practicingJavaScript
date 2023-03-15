8d8ff65c402712cb2c1d4798796e041620584c1c
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-03-14 23:25:38 -0300

    DevAprender: Somar os elementos de uma Array

---
+++ exercicioDois.js
@@ -0,0 +1,15 @@
+//Dada a lista [15,46,75,34,23], imprima o número resultante da soma desses valores.
+
+function sumArray(array) {
+    result = 0
+    for(let i = 0; i < array.length; i++) {
+        result += array[i]
+        
+    }
+    return result
+}
+
+console.log(sumArray([15,46,75,34,23])) //193
+console.log(sumArray([1,2,3,4])) //10
+console.log(sumArray([100,200,198])) //498
+
