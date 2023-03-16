d99ee28d8c4a04d5441efd7283ad8ddcef0f9505
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-03-15 23:20:48 -0300

    DevAprender: Working with loops - For, While, Do...While

---
+++ exercicioCinco.js
@@ -0,0 +1,31 @@
+//For Loop
+
+//Apenas ímpares
+
+for(let i = 0; i <= 10; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
+    }
+}
+
+
+//While Loop
+
+let j = 0
+
+while (j <= 10) {
+    if (j % 2 !== 0) {
+        console.log(j)
+    }
+    j++
+}
+
+//Do...While
+
+let k = 0
+do {
+    console.log("typing...")
+    k++
+} while (k <= 10)
+
+
