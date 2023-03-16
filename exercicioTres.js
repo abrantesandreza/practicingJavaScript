114b6c40cfe9efd9b4e12248f5093df7b55b8bdb
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-03-15 22:44:41 -0300

    DevAprender: Working with IF/Else statements

---
+++ exercicioTres.js
@@ -0,0 +1,15 @@
+// if - else
+
+//Se a hora estiver entre 06:00 até 12:00: Bom dia!
+//Se estiver entre 12:00 até 18:00: Boa tarde!
+//Caso contrario: Boa noite!
+
+let hora = 20;
+
+if (hora > 6 && hora < 12) {
+    console.log("Bom dia!")
+} else if (hora > 12 && hora < 18) {
+    console.log("Boa tarde!")
+} else {
+    console.log ("Boa noite!")
+}
