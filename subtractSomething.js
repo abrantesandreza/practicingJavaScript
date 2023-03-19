991fd0d295f89e945bec3dae781688d82ad3ff0b
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-03-18 21:15:54 -0300

    Lab Methods: Subtract the values of the array

---
+++ subtractSomething.js
@@ -0,0 +1,20 @@
+/* 5) Implemente um método que retorne um array, sem os itens passados por parâmetro depois do array de entrada. 
+Entrada do método ([5,4,3,2,5], 5,3), Resultado do método: [4,2] */
+
+function subtractSomething(arr, valueOne, valueTwo) {
+
+    let newArray = []
+    for(let i = 0; i < arr.length; i++) {
+
+        if(arr[i] !== valueOne && arr[i] !== valueTwo) {
+            newArray.push(arr[i])
+        }
+
+    }
+    return newArray
+
+}
+
+console.log(subtractSomething([5,4,3,2,5], 5,3)) //[4,2]
+console.log(subtractSomething([5,4,3,2,5], 4,2)) //[5,3,5]
+console.log(subtractSomething([6, 2], 4,6)) //[2]
